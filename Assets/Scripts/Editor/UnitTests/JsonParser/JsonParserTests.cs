c488cee9005c036d71d9a37f698f54cc1b6ddee3
Andrés Calandri <andresmcalandri@gmail.com>
2020-06-28 20:22:16 -0300

    Adding a json utility so we can parse the json and its unit tests

---
+++ Assets/Scripts/Editor/UnitTests/JsonParser/JsonParserTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Tests
+{
+    public class JsonParserTests
+    {
+        string testFilePath = "";
+        [SetUp]
+        public void SetUp()
+        {
+            testFilePath = Path.Combine(Application.dataPath, "Scripts", "Editor", "UnitTests", "JsonParser", "testJson.json");
+        }
+
+        [Test]
+        public void GetStringFromJson()
+        {
+            string jsonString = JsonParserUtility.GetStringFromJsonFile(testFilePath);
+            Assert.IsTrue(!string.IsNullOrEmpty(jsonString));
+        }
+
+        [Test]
+        public void GetDictionaryFromJson()
+        {
+            string jsonString = JsonParserUtility.GetStringFromJsonFile(testFilePath);
+            var jsonDictionary = JsonParserUtility.GetDictionaryFromJson<string, object>(jsonString);
+            Assert.IsTrue(jsonDictionary != null && jsonDictionary is Dictionary<string, object>);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/JsonParserUtility.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+public static class JsonParserUtility
+{
+    public static string GetStringFromJsonFile(string jsonFilePath)
+    {
+        StreamReader streamReader = new StreamReader(jsonFilePath);
+        string jsonString = streamReader.ReadToEnd();
+        streamReader.Close();
+        streamReader.Dispose();
+        return jsonString;
+    }
+
+    public static Dictionary<T, J> GetDictionaryFromJson<T, J>(string json)
+    {
+        Dictionary<T, J> jsonDictionary = JsonConvert.DeserializeObject<Dictionary<T, J>>(json);
+        return jsonDictionary;
+    }
+
+    public static List<T> GetListFromJson<T>(string json)
+    {
+        List<T> jsonDictionary = JsonConvert.DeserializeObject<List<T>>(json);
+        return jsonDictionary;
+    }
+}
